ac39dd99f132f72e282bc3247a4a755080bf455a
ThangLV <thang.le.fc@gmail.com>
2020-12-31 17:39:17 +0700

    commit

--- src/core/Common.java
+++ src/core/Common.java
@@ -1,5 +1,20 @@
 package core;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -10,7 +25,62 @@ public class Common {
 		System.setProperty("webdriver.chrome.driver",
 		System.getProperty("user.dir") + "/browserDrivers/chromedriver.exe");
 		webDriver = new ChromeDriver();
+		webDriver.manage().window().maximize();
+		webDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 	
+	public static Map<String, String> getMapData() throws IOException {
+		Map<String, String> testData = new HashMap<String, String>();
+		try{
+			FileInputStream fileInputStream = new FileInputStream("D:\\Auto\\QLNV\\testdata\\testdata.xlsx");
+			Workbook workBook = new XSSFWorkbook(fileInputStream);
+			Sheet sheet = workBook.getSheetAt(0);
+			int lastRowNumber = sheet.getLastRowNum();
+			for(int i = 0; i <= lastRowNumber; i++) {
+				Row row = sheet.getRow(i);
+				Cell keycell = row.getCell(0);
+				String key = keycell.getStringCellValue().trim();
+				Cell valueCell = row.getCell(1);
+				String value = valueCell.getStringCellValue().trim();
+				testData.put(key, value);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return testData;
+	}
+	
+	public static List<Map<String, String>> getTestDataInMap() throws IOException {
+		List<Map<String, String>> testDataAllRows = null;
+		Map<String, String> testData = null;
+		try{
+			FileInputStream fileInputStream = new FileInputStream("D:\\Auto\\QLNV\\testdata\\testdata.xlsx");
+			Workbook workBook = new XSSFWorkbook(fileInputStream);
+			Sheet sheet = workBook.getSheetAt(0);
+			int lastRowNumber = sheet.getLastRowNum();
+			int lastColNumber = sheet.getRow(0).getLastCellNum();
+			List list = new ArrayList();
+			for(int i = 0; i < lastColNumber; i++) {
+				Row row = sheet.getRow(0);
+				Cell cell = row.getCell(i);
+				String rowHeader = cell.getStringCellValue().trim();
+				list.add(rowHeader);
+			}
+			testDataAllRows = new ArrayList<Map<String, String>>();
+			for(int j = 1; j <= lastRowNumber; j++) {
+				Row row = sheet.getRow(j);
+				testData = new TreeMap<String, String>(String.CASE_INSENSITIVE_ORDER);
+				for(int k = 0; k < lastColNumber; k++) {
+					Cell cell = row.getCell(k);
+					String colValue = cell.getStringCellValue().trim();
+					testData.put((String) list.get(k), colValue);
+				}
+				testDataAllRows.add(testData);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return testDataAllRows;
+	}
 	
 }
--- src/test/Testcase.java
+++ src/test/Testcase.java
@@ -1,20 +1,54 @@
 package test;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import java.util.List;
+import java.io.IOException;
+import java.util.Map;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import core.Common;
 import modules.Login;
 
 public class Testcase extends Common {
-	@BeforeMethod
+	private WebDriver driver = null;
+	
+	@BeforeTest
 	public void beforeTest() {
 		initBrowser();
 	}
 	
+	//@Test
+	/*public void readExcelHashMap() {
+		try {
+			Map<String, String> testdata = Common.getMapData();
+			for(Entry<String, String> map: testdata.entrySet()) {
+				System.out.println(map.getKey() + "," + map.getValue());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}*/
+	
 	@Test
-	public void TC01_Phan_xu_ly() {
+	public void readExcelTestData() throws InterruptedException{
+		try {
+			List<Map<String, String>> testDataInMap = Common.getTestDataInMap();
+			((Login) webDriver).getURL();
+			webDriver.findElement(By.xpath("//input[@id='ten_dang_nhap']")).sendKeys(testDataInMap.get(1).get("User"));
+			webDriver.findElement(By.xpath("//input[@id='mat_khau']")).sendKeys(testDataInMap.get(1).get("Password"));
+			webDriver.findElement(By.xpath("//button[contains(text(), 'Đăng nhập')]")).click();
+			Thread.sleep(5000);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//@Test
+	/*public void TC01_Phan_xu_ly() {
 		Login login = new Login(webDriver);
 		login.getURL();
 		
@@ -22,10 +56,10 @@ public class Testcase extends Common {
 		
 		
 		
-	}
+	}*/
 	
-	@AfterMethod
+	@AfterTest
 	public void afterTest() {
-		webDriver.close();
+		webDriver.quit();
 	}
 }
